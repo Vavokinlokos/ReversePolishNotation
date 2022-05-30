8d38c938f611c79d7eb31a62555e427db367d715
AvokinLokos <lerasokolnikova2@gmail.com>
2022-05-30 23:08:45 +0300

    delete comments

--- stack/main.go
+++ stack/main.go
@@ -2,24 +2,21 @@ package stack
 
 type Stack []string
 
-// IsEmpty: check if stack is empty
 func (s *Stack) IsEmpty() bool {
 	return len(*s) == 0
 }
 
-// Push a new value onto the stack
 func (s *Stack) Push(str string) {
-	*s = append(*s, str) // Simply append the new value to the end of the stack
+	*s = append(*s, str)
 }
 
-// Remove and return top element of stack. Return false if stack is empty.
 func (s *Stack) Pop() (string, bool) {
 	if s.IsEmpty() {
 		return "", false
 	} else {
-		index := len(*s) - 1   // Get the index of the top most element.
-		element := (*s)[index] // Index into the slice and obtain the element.
-		*s = (*s)[:index]      // Remove it from the stack by slicing it off.
+		index := len(*s) - 1
+		element := (*s)[index]
+		*s = (*s)[:index]
 		return element, true
 	}
 }
